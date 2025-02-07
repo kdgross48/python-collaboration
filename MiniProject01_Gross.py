e2b7e51f35603263581689bd46ba321201eea1a1
kdgross48 <kdgross.8@gmail.com>
2025-02-07 13:17:01 -0500

    Add files via upload

---
+++ MiniProject01_Gross.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 13:08:23 2025
+
+@author: gross.ke
+"""
+
+def availableCourses ():
+    code code code
+
+#=============================================================================
+def courseRegistration ():
+    code code code
+
+#=============================================================================
+def showRegisteredCourses ():
+    code code code
+    
+#=============================================================================
+def systemExit ():
+    code code code
+
+#=============================================================================
+def main():
+    availableCourses()
+    courseRegistration()
+    showRegisteredCourses()
+    systemExit()
+    
+#=============================================================================
+main()
