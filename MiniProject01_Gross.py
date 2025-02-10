f196ff1eb192740a33887e3ab2090505259328a5
Connor Ng <77085677+eloppole@users.noreply.github.com>
2025-02-10 15:41:48 -0500

    Update MiniProject01_Gross.py

--- MiniProject01_Gross.py
+++ MiniProject01_Gross.py
@@ -2,30 +2,43 @@
 """
 Created on Fri Feb  7 13:08:23 2025
 
-@author: gross.ke
 """
 
-def availableCourses ():
-    code code code
+classes = ["EECE2140", "EECE2150", "EECE2160", "MATH2321", "MATH2341"]
 
+def availableCourses ():
+    index = 1
+    for i in classes:
+        print(""+str(index)+". "+i)
+        index = index + 1
+        
 #=============================================================================
 def courseRegistration ():
-    code code code
+    
 
 #=============================================================================
 def showRegisteredCourses ():
-    code code code
+    print("Skibidi3")
     
 #=============================================================================
 def systemExit ():
-    code code code
+    print("Skibidi4")
 
 #=============================================================================
 def main():
-    availableCourses()
-    courseRegistration()
-    showRegisteredCourses()
-    systemExit()
+    name = (input("Input your name: "))
+    decision = int(input("\nWhat would you like to do, " + name + "?\nYou can view available courses (1),\nregister for a course (2),\nview registered courses (3),\nor exit the system (4)?\nYou are limited to a maximum of 3 courses.\n\nDecision: "))
+    
+    if decision == 1: 
+        availableCourses()
+    elif decision == 2: 
+        courseRegistration()
+    elif decision == 3: 
+        showRegisteredCourses()
+    elif decision == 4: 
+        systemExit()
+    else:
+        decision = input("Invalid number.")
     
 #=============================================================================
 main()
