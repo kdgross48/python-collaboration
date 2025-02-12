1b1d19ceb81d452214db6554f8987f0866d36b10
Amogh <amogh251106@gmail.com>
2025-02-12 13:26:21 -0500

    Update MiniProject01_Gross.py

--- MiniProject01_Gross.py
+++ MiniProject01_Gross.py
@@ -1,24 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  7 13:08:23 2025
-
-"""
 
 classes = ["EECE2140", "EECE2150", "EECE2160", "MATH2321", "MATH2341"]
+class_reg =[]
 
 def availableCourses ():
-    index = 1
+    class_num = 1 
     for i in classes:
-        print(""+str(index)+". "+i)
-        index = index + 1
+        print(str(class_num)+ ". "+i)
+        class_num = class_num + 1
         
 #=============================================================================
 def courseRegistration ():
-    
+    course1 = int(input("Please choose a course(enter its number):"))
+    class_reg.append(course1)
+    course2 = int(input("Please choose a course(enter its number)"))
+    class_reg.append(course2)
+    course3 = int(input("Please choose a course(enter its number)"))
+    class_reg.append(course3)
 
 #=============================================================================
 def showRegisteredCourses ():
-    print("Skibidi3")
+    class_num = 1 
+    for i in class_reg:
+        print(str(class_num)+ ". "+i)
+        class_num = class_num + 1
     
 #=============================================================================
 def systemExit ():
@@ -26,6 +30,7 @@ def systemExit ():
 
 #=============================================================================
 def main():
+    
     name = (input("Input your name: "))
     decision = int(input("\nWhat would you like to do, " + name + "?\nYou can view available courses (1),\nregister for a course (2),\nview registered courses (3),\nor exit the system (4)?\nYou are limited to a maximum of 3 courses.\n\nDecision: "))
     
