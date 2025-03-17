1a12f19d97d19b8373eab8a5b4bdbec95443284e
Connor Ng <77085677+eloppole@users.noreply.github.com>
2025-03-17 16:02:39 -0400

    Update MiniProject01_Gross.py

--- MiniProject01_Gross.py
+++ MiniProject01_Gross.py
@@ -1,51 +1,80 @@
+import cmath
 
-classes = ["EECE2140", "EECE2150", "EECE2160", "MATH2321", "MATH2341"]
-class_reg =[]
+class Firstorder:
+    def exact():
+        pass
+    def exact():
+        pass
+    def exact():
+        pass
+    def exact():
+        pass
+    def exact():
+        pass    
 
-def availableCourses ():
-    class_num = 1 
-    for i in classes:
-        print(str(class_num)+ ". "+i)
-        class_num = class_num + 1
+class Secondorder:
+    def homogeneous():
+        print("\n\nGiven the form:\nay\" + by' + cy = 0\n")
+        a = int(input("Input your 'a' value: "))
+        b = int(input("Input your 'b' value: "))
+        c = int(input("Input your 'c' value: "))
+                
+        try:
+            x1 = -b+cmath.sqrt((b**2)-(4*a*c))/(2*a)
+            x2 = -b-cmath.sqrt((b**2)-(4*a*c))/(2*a)
+        except:
+            print("Error")
+           
+        try:
+            x1 = str(x1).replace("+0j","")
+            x2 = str(x2).replace("+0j","")
+        except:
+            print("Error")
+            
+        solution_list = [x1,x2]
+        return solution_list
+    def homogeneous_output():
+        my_list = homogenous()
+        x1 = my_list[0]
+        x2 = my_list[1]
+        if x1 != x2 and "j" not in str(x1):
+            print("The general form of a solution for this is:\ny=c1e^(at)+c2e^(bt)")
+            print(f"Your first solution is c1(e^(({x1})t))")
+            print(f"Your second solution is c2(e^(({x2})t))")
+            print(f"Using the superposition principle, the general solution to the provided differential equation is:\nc1(e^(({x1})t))+c2(e^(({x2})t))")
+        if x1 == x2:
+            print("The general form of a solution for this is:\ny=c1e^(at)+c2te^(bt)")
+            print(f"Your first solution is c1(e^(({x1})t))")
+            print(f"Your second solution is c2t(e^(({x1})t))")
+            print(f"Using the superposition principle, the general solution to the provided differential equation is:\nc1(e^(({x1})t))+c2t(e^(({x1})t))")
+        if "j" in str(x1):
+            temp = str(x1).split("+")
+            temp[1] = temp[1].replace("j","")
+            print("The general form of a solution for this is:\n(e^(at))[c1sin(bt)+c2cos(bt)]")
+            print(f"Your solution is:\n(e^({temp[0]}t))[c1sin({temp[1]}t)+c2cos({temp[1]}t)]")
+    def nonhomogeneous():
         
-#=============================================================================
-def courseRegistration ():
-    course1 = int(input("Please choose a course(enter its number):"))
-    class_reg.append(classes[course1-1])
-    course2 = int(input("Please choose a course(enter its number)"))
-    class_reg.append(classes[course2-1])
-    course3 = int(input("Please choose a course(enter its number)"))
-    class_reg.append(classes[course3-1])
-
-#=============================================================================
-def showRegisteredCourses ():
-    class_num = 1 
-    for i in class_reg:
-        print(str(class_num)+ ". "+i)
-        class_num = class_num + 1
     
-#=============================================================================
-def systemExit ():
-    print("You just exited the program, thank you!")
-
-#=============================================================================
+class Laplace:
+    pass
+    
 def main():
+    Secondorder.homogeneous()
     
-    name = (input("Input your name: "))
-    decision = 0
-    while decision != 4:
-        decision = int(input("\nWhat would you like to do, " + name + "?\nYou can view available courses (1),\nregister for a course (2),\nview registered courses (3),\nor exit the system (4)?\nYou are limited to a maximum of 3 courses.\n\nDecision: "))
-        if decision == 1: 
-            availableCourses()
-        elif decision == 2: 
-            courseRegistration()
-        elif decision == 3: 
-            showRegisteredCourses()
-        elif decision == 4:
-            systemExit()
+    order = int(input("What order is your differential equation? (1, 2, ?) [Respond with a question mark if you don't understand the question!]:\n"))
+    if (order == 1):
+        print("")
+    if (order == 2):
+        print("Second order equations to be solved will be in the form:\nay\" + by' + cy = f(x)\n")
+        function = int(input("Input f(x): "))
+        if function == 0:
+            Secondorder.homogeneous()
+            Secondorder.homogeneous_output()
         else:
-            decision = input("Invalid number.")
+            Secondorder.nonhomogeneous()
+    if (order == 3):
+        print("stuff")
         
-    
-#=============================================================================
+    return 0
+
 main()
